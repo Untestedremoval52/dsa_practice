91dffe65a07659470d775feec153a6b487fcc535
Garv Bhardwaj <garvbhardwaj0609@gmail.com>
2026-08-20 23:51:23 +0530

    Add initial setup of the circular doubly linked list and display functions in both directions

---
+++ linked_lists/circular_doubly_linked_list.py
@@ -0,0 +1,34 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+class CircularDoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+    def is_empty(self):
+        return self.head is None
+    def display_forward(self):
+        if self.is_empty() == True:
+            print("The list is empty, please try again!")
+            return
+        current_node = self.head
+        while True:
+            print(current_node.data, end = " ")
+            current_node = current_node.next
+            if current_node == self.head:
+                break
+        print()
+    def display_backward(self):
+        if self.is_empty() == True:
+            print("The list is empty, please try again!")
+            return
+        current_node = self.tail
+        while True:
+            print(current_node.data, end = " ")
+            current_node = current_node.prev
+            if current_node == self.tail:
+                break
+        print()
