950ac132342817f2f97c1103c2effa66b4bd2957
Garv Bhardwaj <garvbhardwaj0609@gmail.com>
2026-08-23 14:45:58 +0530

    Add count the number of nodes in a circular singly linked list function

--- linked_lists/linked_list_problems.py
+++ linked_lists/linked_list_problems.py
@@ -34,9 +34,53 @@ class Singly_Linked_List:
         for _ in range(length // 2):
             current_node = current_node.next
         print(f"The middle element is {current_node.data}")
-sll = Singly_Linked_List()
-sll.insert_beginning(10)
-sll.insert_beginning(20)
-sll.insert_beginning(40)
-sll.insert_beginning(30)
-sll.search_middle_element()
+class Circular_Singly_Linked_List:
+    def __init__(self):
+        self.head = None
+        self.length = 0
+    def is_empty(self):
+        return self.head is None
+    def display_list(self):
+        if self.is_empty() == True:
+            print("The list is empty")
+        else:
+            current_node = self.head
+            while True:
+                print(current_node.data, end = " ")
+                current_node = current_node.next
+                if current_node == self.head:
+                    break
+            print()
+    def insert_beginning(self, value):
+        node = Node(value)
+        if self.is_empty() == True:
+            self.head = node
+            node.next = self.head
+        else:
+            current_node = self.head
+            while current_node.next != self.head:
+                current_node = current_node.next
+            current_node.next = node
+            node.next = self.head
+            self.head = node
+        self.length +=1
+        self.display_list()
+    def count_nodes(self):
+        current_node = self.head
+        count = 0
+        if self.is_empty() == True:
+            print("The list is empty, please try again!")
+            return
+        while True:
+            current_node = current_node.next
+            count += 1
+            if current_node == self.head:
+                break
+        print(f"The total number of nodes are: {count}")
+cdll = Circular_Singly_Linked_List()
+cdll.count_nodes()
+cdll.insert_beginning(10)
+cdll.count_nodes()
+cdll.insert_beginning(20)
+cdll.insert_beginning(30)
+cdll.count_nodes()
