8fc8e50e673968ba173f620e7e348bea9e7e2484
Garv Bhardwaj <garvbhardwaj0609@gmail.com>
2026-08-27 21:12:47 +0530

    Add function to implement queues using lists

---
+++ queues/queues_using_lists.py
@@ -0,0 +1,37 @@
+class QueuesUsingLists:
+    def __init__(self):
+        self.queue = []
+    def is_empty(self):
+        return len(self.queue) == 0
+    def display(self):
+        if self.is_empty() == True:
+            print("The list is empty, please try again!")
+            return
+        print(self.queue)
+    def enqueue(self, data):
+        self.queue.append(data)
+        self.display()
+    def dequeue(self):
+        if self.is_empty() == True:
+            print("The list is empty, please try again!")
+            return
+        popped = self.queue.pop(0)
+        self.display()
+        return popped
+    def frontpeek(self):
+        if self.is_empty() == True:
+            print("The list is empty, please try again!")
+            return
+        return self.queue[0]
+    def rearpeek(self):
+        if self.is_empty() == True:
+            print("The list is empty, please try again!")
+            return
+        return self.queue[-1]
+qul = QueuesUsingLists()
+qul.enqueue(10)
+qul.enqueue(20)
+qul.enqueue(30)
+qul.dequeue()
+qul.dequeue()
+qul.frontpeek()
